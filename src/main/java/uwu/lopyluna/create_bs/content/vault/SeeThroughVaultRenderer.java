2f0ed5f44afd5ed08811295dea24bdae5aecd83d
Neha YoruNina <836193631@qq.com>
2025-04-05 23:11:35 +0800

    port create 6.0

--- src/main/java/uwu/lopyluna/create_bs/content/vault/SeeThroughVaultRenderer.java
+++ src/main/java/uwu/lopyluna/create_bs/content/vault/SeeThroughVaultRenderer.java
@@ -1,15 +1,15 @@
 package uwu.lopyluna.create_bs.content.vault;
 
-import com.jozufozu.flywheel.util.transform.TransformStack;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
-import com.simibubi.create.foundation.utility.AnimationTickHolder;
-import com.simibubi.create.foundation.utility.VecHelper;
+import net.createmod.catnip.animation.AnimationTickHolder;
+import net.createmod.catnip.math.VecHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import dev.engine_room.flywheel.lib.transform.TransformStack;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
@@ -39,15 +39,14 @@ public class SeeThroughVaultRenderer extends SmartBlockEntityRenderer<TieredVaul
         RandomSource r = RandomSource.create((long) pos.hashCode() * (slot + 1L));
 
         Vec3 vec = VecHelper.offsetRandomly(Vec3.ZERO, r, 0.35f);
-        float multiplier = r.nextIntBetweenInclusive(150, 500) / 100f;
         float time = AnimationTickHolder.getRenderTime() / 20;
-        float angle = (((time * 10f) * multiplier) % 360f) * (r.nextBoolean() ? 1f : -1f);
+        float angle = time % 360f;
         float timeOffset = (slot * 57.2958f) % 360f;
         float bob = (float) Math.sin((time + timeOffset) * Math.PI) * 0.05f;
 
         ms.translate(vec.x + 0.5, vec.y + 0.35 + bob, vec.z + 0.5);
-        TransformStack.cast(ms).rotateY(angle);
-        TransformStack.cast(ms).scale(0.75f);
+        TransformStack.of(ms).rotateY(angle);
+        TransformStack.of(ms).scale(0.75f);
         mc.getItemRenderer().renderStatic(stack, ItemDisplayContext.GROUND, light, overlay, ms, buffer, mc.level, 0);
         ms.popPose();
     }
